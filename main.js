8d2bb4ab32938eb56aad0fd55757b99d65cde15b
BOULET Pierre <pierre.boulet@maif.fr>
2026-05-05 11:44:17 +0200

    feat: addTask arrow function created

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Hello world !");
+// Création d'un tableau d'objets, chaque objet étant une tâche
+let tasksOfTheDay = [
+ {title: 'Faire les courses', done: false },
+ {title: 'Nettoyer la maison', done: true },
+ {title: 'Planter le jardin', done: false }
+];
+
+// Déclaration de la méthode de façon "abstraite" pour qu'elle s'adapte à n'impoprte quel paramètre qui lui seront passés.
+const addTask = (taskList, newTask) => {
+ const newTaskList = [...taskList, newTask];
+ return newTaskList;
+};
+
+addTask(tasksOfTheDay, {title: 'Faire mes devoirs', done: false});
+
+console.log(addTask(tasksOfTheDay, {title: 'Faire mes devoirs', done: false}));
