42866c74fe8609d76b96103d8a694eb53e4be8c3
BOULET Pierre <pierre.boulet@maif.fr>
2026-05-07 10:49:52 +0200

    feat: added comments, wrote function to remove one task in a list

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
 // Création d'un tableau d'objets, chaque objet étant une tâche
-let tasksOfTheDay = [
- {title: 'Faire les courses', done: false },
- {title: 'Nettoyer la maison', done: true },
- {title: 'Planter le jardin', done: false }
+const tasksOfTheDay = [
+ { title: 'Faire les courses', done: false },
+ { title: 'Nettoyer la maison', done: true },
+ { title: 'Planter le jardin', done: false }
 ];
 
-// Déclaration de la méthode de façon "abstraite" pour qu'elle s'adapte à n'impoprte quel paramètre qui lui seront passés.
-const addTask = (taskList, newTask) => {
- const newTaskList = [...taskList, newTask];
- return newTaskList;
+//* ============= Ajouter une tâche à la liste =============
+
+// Déclaration de la variable qui accueillera mon résultat
+// let newTaskList;
+
+// Déclaration de la méthode de façon "abstraite" pour qu'elle s'adapte à n'importe quels paramètres qui lui seront passés.
+// const addTask = (taskList, newTask) => { newTaskList = [...taskList, newTask]; };
+
+// Appel de la fonction pour la tester
+// addTask(tasksOfTheDay, { title: 'Faire mes devoirs', done: false });
+
+// Vérification du résultat dans la console
+// console.log(newTaskList);
+
+//* ============= Supprimer une tâche de la liste =============
+
+// Déclaration de la variable qui accueillera mon résultat
+let listAfterRemoval;
+
+// Fonction fléchée qui retourne la liste de tous les items n'ayant pas pour titre le paramètre 'title'
+const removeTask = (taskList, title) => {
+ return listAfterRemoval = taskList.filter((task) => title !== task.title);
 };
 
-addTask(tasksOfTheDay, {title: 'Faire mes devoirs', done: false});
+// Appel de la fonction pour la tester
+removeTask(tasksOfTheDay, "Faire les courses");
 
-console.log(addTask(tasksOfTheDay, {title: 'Faire mes devoirs', done: false}));
+// Vérification du résultat dans la console
+console.log(listAfterRemoval);
+
